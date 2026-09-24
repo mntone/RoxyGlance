3e0f02ee7a94873c7cf7ad6732c727e716d3eb24
mntone <901816+mntone@users.noreply.github.com>
2026-09-24 13:19:25 +0900

    feat(winui3): embed executable version information
    - define shared file and product version properties
    - add version resource metadata to the WinUI 3 executable

---
+++ src/winui3/Resources/resource.h
@@ -0,0 +1,38 @@
+#ifndef RC_INVOKED
+#define RC_INVOKED
+#endif
+
+#define STRINGIZE_DETAIL(x) #x
+#define STRINGIZE(x) STRINGIZE_DETAIL(x)
+
+#ifdef ROXYG_VER_FILE_VERSION
+#define VER_FILE_VERSION            ROXYG_VER_FILE_VERSION
+#else
+#define VER_FILE_VERSION            0,0,0,0
+#endif
+#ifdef ROXYG_VER_FILE_VERSION_STRING
+#define VER_FILE_VERSION_STRING     STRINGIZE(ROXYG_VER_FILE_VERSION_STRING)
+#else
+#define VER_FILE_VERSION_STRING     "0.0.0.0"
+#endif
+#ifdef ROXYG_VER_PRODUCT_VERSION
+#define VER_PRODUCT_VERSION         ROXYG_VER_PRODUCT_VERSION
+#else
+#define VER_PRODUCT_VERSION         0,0,0,0
+#endif
+#ifdef ROXYG_VER_PRODUCT_VERSION_STRING
+#define VER_PRODUCT_VERSION_STRING  STRINGIZE(ROXYG_VER_PRODUCT_VERSION_STRING)
+#else
+#define VER_PRODUCT_VERSION_STRING  "0.0.0-unknown"
+#endif
+
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40400
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           104
+#endif
+#endif
